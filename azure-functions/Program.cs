c25c32e7052974e8315da90b47ed0e41f7025e8a
Vigouroux Johanne <102874093+johannvig@users.noreply.github.com>
2024-12-19 19:59:11 -0500

    Rename Program.cs to azure-functions/Program.cs

--- azure-functions/Program.cs
+++ azure-functions/Program.cs
@@ -2,7 +2,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Azure.Functions.Worker.Configuration;
 
 var host = new HostBuilder()
-	.ConfigureFunctionsWorkerDefaults() // ou ConfigureFunctionsWebApplication() si vous utilisez ce modèle
+	.ConfigureFunctionsWorkerDefaults() // ou ConfigureFunctionsWebApplication() si vous utilisez ce modÃ¨le
 	.Build();
 
 host.Run();
