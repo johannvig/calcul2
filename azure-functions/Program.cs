802d8404f451f546c851baf0458bf14fd76c025f
Vigouroux Johanne <102874093+johannvig@users.noreply.github.com>
2024-12-19 23:15:06 -0500

    Update Program.cs

--- azure-functions/Program.cs
+++ azure-functions/Program.cs
@@ -1,8 +1,12 @@
-using Microsoft.Extensions.Hosting;
-using Microsoft.Azure.Functions.Worker.Configuration;
+using Microsoft.Extensions.Hosting; // Importation du namespace pour configurer et démarrer un hôte générique
+using Microsoft.Azure.Functions.Worker.Configuration; // Importation du namespace pour la configuration des Azure Functions Worker
 
+// Création d'un hôte générique pour exécuter les Azure Functions
 var host = new HostBuilder()
-	.ConfigureFunctionsWorkerDefaults() // ou ConfigureFunctionsWebApplication() si vous utilisez ce modèle
-	.Build();
+	// Configure les paramètres par défaut pour une application Azure Functions Worker
+	.ConfigureFunctionsWorkerDefaults() 
+	// Note : Utilisez `.ConfigureFunctionsWebApplication()` si vous travaillez avec un modèle basé sur une application Web.
+	.Build(); // Construit l'hôte avec les paramètres définis
 
+// Démarre l'hôte et exécute l'application Azure Functions
 host.Run();
